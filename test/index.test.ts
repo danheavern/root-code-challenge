13f6de0a46fc1dccae0695edb9eab22fcbfe0e4f
danheavern <danheavern@gmail.com>
2021-01-13 18:01:55 -0500

    Update tests to use Trip constructor

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,6 +1,6 @@
-import { parseCommand, registerDriver } from "../src";
+import { addTrip, parseCommand, registerDriver } from "../src";
 import { DRIVER_COMMAND, TRIP_COMMAND } from "../src/constants"
-import { Driver } from "../src/model";
+import { Driver, Trip } from "../src/model";
 
 describe('index.ts - main body of code', () => {
     test('parseCommand returns DRIVER_COMMAND if first word is Driver', () => {
@@ -12,14 +12,29 @@ describe('index.ts - main body of code', () => {
     test('parseCommand should return null if no valid command present', () => {
         expect(parseCommand('Drip is a combination of Driver and Trip')).toBeNull();
     });
-    test('registerDriver returns an array of length drivers.length + 1', () => {
+    test('registerDriver should return an array of length drivers.length + 1', () => {
         expect(registerDriver([], 'Test')).toHaveLength(1);
     });
-    test('registerDriver returns an array of Driver objects', () => {
+    test('registerDriver should return an array of Driver objects', () => {
         const res = registerDriver([], 'Test');
         res.forEach(d => expect(d).toBeInstanceOf(Driver));
     });
-    test('registerDriver returns an array which contains a Driver with the name provided', () => {
+    test('registerDriver should return an array which contains a Driver with the name provided', () => {
         expect(registerDriver([], 'Test').some(d => d.name === 'Test')).toBe(true);
+    });
+    test('addTrip should return an array of length trips.length + 1', () => {
+        expect(addTrip([], 'Test', 0, 60, 60)).toHaveLength(1);
+    });
+    test('addTrip should return an array of Trip objects', () => {
+        const res = addTrip([], 'Test', 0, 60, 60);
+        res.forEach(d => expect(d).toBeInstanceOf(Trip));
+    });
+    test('addTrip should return an array which contains a Trip with the values provided', () => {
+        expect(addTrip([], 'Test', 0, 60, 60).some(t => (
+            t.driverName === 'Test'
+            && t.start === 0
+            && t.end === 60
+            && t.dist === 60
+            ))).toEqual(true);
     });
 })
