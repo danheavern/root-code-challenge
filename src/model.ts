9219a8902165b33a327f83849ca69f70b48b69cb
danheavern <danheavern@gmail.com>
2021-01-13 14:33:18 -0500

    create Driver and Trip models

---
+++ src/model.ts
@@ -0,0 +1,23 @@
+export class Trip {
+    driverName: string; //name of driver
+    start: number;      //start time in minutes
+    end: number;        //end time in minutes
+    dist: number;       //distance travelled in miles
+
+    /**
+     * Returns true if 5mph <= speed <= 100mph
+     */
+    isValid(){}
+}
+
+export class Driver {
+    name: string;
+    milesDriven: number;    //total miles driven
+    avgSpeed: number;       //average speed in mph
+
+    /**
+     * Update avgSpeed using a new trip speed
+     * @param tripSpeed speed in mph to include in avg
+     */
+    updateAvgSpeed(tripSpeed: number){}
+}
