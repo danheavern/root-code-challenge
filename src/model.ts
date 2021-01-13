8b21689aea7e7bdd2da7d0ba96f0d6e5f1edc019
danheavern <danheavern@gmail.com>
2021-01-13 16:36:24 -0500

    fix bug in isValid by converting to hours, implement updateAvgSpeed

--- src/model.ts
+++ src/model.ts
@@ -8,18 +8,28 @@ export class Trip {
      * Returns true if 5mph <= speed <= 100mph
      */
     isValid(): Boolean {
-        return this.dist / (this.end - this.start) <= 100 && this.dist / (this.end - this.start) >= 5;
+        return this.dist / (this.end - this.start) * 60 <= 100 && this.dist / (this.end - this.start) * 60 >= 5;
     }
 }
 
 export class Driver {
     name: string;
+    totalTrips: number;      //total trips taken - used in updating average speed
     milesDriven?: number;    //total miles driven
     avgSpeed?: number;       //average speed in mph
 
     /**
-     * Update avgSpeed using a new trip speed
+     * Update *avgSpeed* using a new trip speed, increments totalTrips
      * @param tripSpeed speed in mph to include in avg
      */
-    updateAvgSpeed(tripSpeed: number){}
+    updateAvgSpeed(tripSpeed: number){
+        this.totalTrips = this.totalTrips ? this.totalTrips + 1 : 1;
+        this.avgSpeed = this.avgSpeed ? this.avgSpeed * ((this.totalTrips - 1) / this.totalTrips) + (tripSpeed / this.totalTrips) : tripSpeed;
+    }
+
+    /**
+     * Updates *milesDriven*, adding *miles* to the total 
+     * @param miles miles to be added
+     */
+    updateMilesDriven(miles: number){}
 }
