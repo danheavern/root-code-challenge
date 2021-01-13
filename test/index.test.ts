61bac49485fde6bd01134cb9d433445e9f5c7970
danheavern <danheavern@gmail.com>
2021-01-13 17:37:06 -0500

    Update registerDriver tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,6 @@
-import { parseCommand } from "../src";
+import { parseCommand, registerDriver } from "../src";
 import { DRIVER_COMMAND, TRIP_COMMAND } from "../src/constants"
+import { Driver } from "../src/model";
 
 describe('index.ts - main body of code', () => {
     test('parseCommand returns DRIVER_COMMAND if first word is Driver', () => {
@@ -10,5 +11,15 @@ describe('index.ts - main body of code', () => {
     });
     test('parseCommand should return null if no valid command present', () => {
         expect(parseCommand('Drip is a combination of Driver and Trip')).toBeNull();
+    });
+    test('registerDriver returns an array of length drivers.length + 1', () => {
+        expect(registerDriver([], 'Test')).toHaveLength(1);
+    });
+    test('registerDriver returns an array of Driver objects', () => {
+        const res = registerDriver([], 'Test');
+        res.forEach(d => expect(d).toBeInstanceOf(Driver));
+    });
+    test('registerDriver returns an array which contains a Driver with the name provided', () => {
+        expect(registerDriver([], 'Test').some(d => d.name === 'Test')).toBe(true);
     });
 })
