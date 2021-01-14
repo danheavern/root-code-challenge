68be4e0df9b4883ffaeff6c2abbef47da65a5f06
danheavern <danheavern@gmail.com>
2021-01-14 12:47:07 -0500

    implement main function, add comments

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { DRIVER_COMMAND, TRIP_COMMAND } from "./constants";
 import { Driver, Trip } from "./model";
-import readline = require('readline');
+import * as readline from 'readline';
+
+main();
 
 /**
  * Returns the command used in a given line of input, and null if there is no valid command
@@ -19,9 +21,7 @@ export function parseCommand(line: string): string {
  * @param line string
  */
 export function parseDriverCommand(line: string): Driver {
-    const driver = new Driver;
-    driver.name = line.split(' ')[1];
-    return driver;
+    return new Driver(line.split(' ')[1]);
 }
 
 /**
@@ -59,38 +59,69 @@ export function registerDriver(drivers: Array<Driver>, driver: Driver): Array<Dr
 
 /**
  * Returns given array with new trip appended
- * @param driverName Name of driver
  * @param trips Array of trips to add to
- * @param start Start time in minutes
- * @param end End time in minutes
- * @param dist Distance in miles
+ * @param trip Trip onject to append to return array
  */
 export function addTrip(trips: Array<Trip>, trip: Trip): Array<Trip> {
     return [...trips, trip];
 }
 
-const drivers: Array<Driver> = [];
-const trips: Array<Trip> = [];
+/**
+ * takes a trip and returns array drivers with updated Driver named in trip
+ * @param drivers array of drivers
+ * @param trip new trip to update drivers
+ */
+export function updateDrivers(drivers: Array<Driver>, trip: Trip): Array<Driver> {
+    const tripDriver = drivers.find(d => d.name === trip.driverName);
+    tripDriver.updateDriver(trip);
+    return drivers.map(d => d.name === tripDriver.name ? tripDriver : d);
+}
 
-var rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+export function generateReport(drivers: Array<Driver>) {
+    drivers.sort((a, b) => b.milesDriven - a.milesDriven);
+    drivers.forEach(driver => {
+        if(driver.milesDriven > 0) {
+            console.log(`${driver.name}: ${Math.round(driver.milesDriven)} miles @ ${Math.round(driver.avgSpeed)} mph`);
+        } else {
+            console.log(`${driver.name}: 0 miles`);
+        }
+    })
+}
 
-rl.on('line', (line) => {
-    switch(parseCommand(line)){
-        case DRIVER_COMMAND: {
-            // registerDriver(drivers, parseDriverCommand(line))
-        }
-        case TRIP_COMMAND: {
+export default function main() {
+    var drivers: Array<Driver> = [];
+    var trips: Array<Trip> = [];
 
+    // setup readline interface
+    // stdin and stdout will be the input/output
+    // we use the crlfDelay option to recognize all instances of CR LF
+    // ('\r\n') in input.txt as a single line break.
+    // terminal: false allows us to suppress the input from being printed, and we can write the output using console.log
+    var rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        crlfDelay: Infinity,
+        terminal: false
+    });
+
+    rl.on('line', (line) => {
+        const command = parseCommand(line);
+        if(command === DRIVER_COMMAND) {
+            drivers = registerDriver(drivers, parseDriverCommand(line));
+        } else if(command === TRIP_COMMAND) {
+            const trip = parseTripCommand(line);
+            //trip is valid - add to trips, and update drivers
+            if(trip.isValid) {
+                trips = addTrip(trips, trip);
+                drivers = updateDrivers(drivers, trip);
+            }
+        } else {
+            //returned null - invalid command
+            console.log('Encountered invalid command.');
         }
-        //returned null - invalid command
-        default: {
-            rl.write('')
-        }
-    }
-}).on('close', () => {
-    //done reading
-    process.exit(0);
-})
+    }).on('close', () => {
+        //done reading
+        generateReport(drivers);
+        process.exit(0);
+    })
+}
